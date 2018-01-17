03fbf03d69bf2c6a936f8d60ddc3acfea6e1ffd2
Tim Cutting <tim.cutting@internal.gfm.co.uk>
2018-01-17 16:11:36 +0000

    Moved JS to js.js

--- js/js.js
+++ js/js.js
@@ -1,1 +1,6 @@
-// Nothing here yet.
+// Nav Stuff...
+document.getElementById('nav-items').addEventListener('click', function(e){
+    if( e.target.classList.contains('sub-menu') ){
+        e.target.classList.toggle('closed');
+    }
+});
