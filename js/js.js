be86a35cd99b06b4c935882821389e1b334ba508
timcutting-gfm <34442834+timcutting-gfm@users.noreply.github.com>
2018-01-19 19:04:37 +0000

    Add animated menu icon, update for @supports query.

--- js/js.js
+++ js/js.js
@@ -1,6 +1,14 @@
 // Nav Stuff...
 document.getElementById('nav-items').addEventListener('click', function(e){
     if( e.target.classList.contains('sub-menu') ){
+        document.querySelectorAll('.sub-menu').forEach(element => {
+            element.classList.add('closed');
+        });
         e.target.classList.toggle('closed');
     }
+});
+// Nav UI...
+document.getElementById('nav-open').addEventListener('click',function(){
+    document.getElementById('nav').classList.toggle('open');
+    this.classList.toggle('active');
 });
